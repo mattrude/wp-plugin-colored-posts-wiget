d93fc13db3ccb6f77419f4a09c5e00a5c263a82e
Matt Rude <github@mattrude.com>
2009-11-13 03:56:33 +0000

    added new plugin info

---
+++ colored-posts-wiget.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Colored Posts Wiget
+Plugin URI: http://mattrude.com/projects/
+Description:
+Version: 0.1
+Author: Matt Rude
+Author URI: http://mattrude.com/
+*/
+
+?>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Colored Posts Wiget ===
+Contributors: mattrude
+Tags: 
+Requires at least: 2.9
+Tested up to: 2.9
+Stable tag: 0.1
+
+== Description ==
+
