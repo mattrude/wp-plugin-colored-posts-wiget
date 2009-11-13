7d11928330b77acba4025cd7f39a0f472fc0706f
Matt Rude <github@mattrude.com>
2009-11-13 04:39:37 +0000

    add the posts per page function

--- colored-posts-wiget.php
+++ colored-posts-wiget.php
@@ -9,16 +9,40 @@ Author URI: http://mattrude.com/
 */
 
 class colored_posts_wiget extends WP_Widget {
-  function colored_posts() {
+  function colored_posts_wiget() {
+    $currentLocale = get_locale();
+    if(!empty($currentLocale)) {
+      $moFile = dirname(__FILE__) . "/languages/colored-posts-wiget_" .  $currentLocale . ".mo";
+      if(@file_exists($moFile) && is_readable($moFile)) load_textdomain('', $moFile);
+    }
+    $colored_posts_name = __('Colored Posts Wiget', 'colored-posts-wiget');
+    $colored_posts_description = __('Colored Posts Wiget for WordPress', 'colored-posts-wiget');
+    $widget_ops = array('classname' => 'colored_posts_wiget', 'description' => $colored_posts_description );
+    $this->WP_Widget('colored_posts_wiget', $colored_posts_name, $widget_ops);
   }  
 
   function widget($args, $instance) {
+    extract($args);
+    $posts_per_page = empty($instance['posts_per_page']) ? '&nbsp;' : apply_filters('post_per_page', $instance['posts_per_page']);
+    query_posts('posts_per_page=$posts_per_page');
+    if (have_posts()) : while (have_posts()) : the_post();
+      the_content();
+    endwhile;
+    endif;
+    wp_reset_query();
   }
   
   function update($new_instance, $old_instance) {
+    $instance = $old_instance;
+    $instance['posts_per_page'] = strip_tags($new_instance['posts_per_page']);
+    return $instance;
   }
 
   function form($instance) {
+    $title = strip_tags($instance['posts_per_page']);
+    ?>
+    <p><label for="<?php echo $this->get_field_id('posts_per_page'); ?>"><?php _e('Posts per Page', 'colored-posts-wiget')?>:<input class="widefat" id="<?php echo $this->get_field_id('posts_per_page'); ?>" name="<?php echo $this->get_field_name('posts_per_page'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+    <?php
   }
   
 }
