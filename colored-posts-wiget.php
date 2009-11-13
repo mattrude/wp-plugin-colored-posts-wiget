988d143d05e39e4a8f4d6e82515c02ad810fdced
Matt Rude <github@mattrude.com>
2009-11-13 04:06:40 +0000

    add the framework for wiget

--- colored-posts-wiget.php
+++ colored-posts-wiget.php
@@ -8,4 +8,24 @@ Author: Matt Rude
 Author URI: http://mattrude.com/
 */
 
+class colored_posts_wiget extends WP_Widget {
+  function colored_posts() {
+  }  
+
+  function widget($args, $instance) {
+  }
+  
+  function update($new_instance, $old_instance) {
+  }
+
+  function form($instance) {
+  }
+  
+}
+
+add_action('widgets_init', 'widget_colored_posts_init');
+function widget_colored_posts_init() {
+        register_widget('colored_posts_wiget');
+}
+
 ?>
